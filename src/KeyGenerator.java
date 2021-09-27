720f47c215bca4c52440e8319d80f99cf4eac194
Goku900 <lizeth.a.nava@outlook.com>
2021-09-26 22:03:17 -0600

    Created RSA keys for X

--- src/KeyGenerator.java
+++ src/KeyGenerator.java
@@ -1,14 +1,24 @@
-public class KeyGenerator {
-    //TODO Create pair of RSA public and private keys for X
+import java.security.*;
 
-    //TODO Create pair of RSA public and private keys for Y
+public class KeyGenerator{
+    public static void main(String[] args) throws Exception {
+        //TODO Create pair of RSA public and private keys for X
+        SecureRandom random = new SecureRandom();
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(1024, random);
+        KeyPair pair = generator.generateKeyPair();
+        Key pubKeyX = pair.getPublic();
+        Key privKeyX = pair.getPrivate();
 
-    //TODO Get the modulus and exponents for each RSA public and private keys
+        //TODO Create pair of RSA public and private keys for Y
 
-    //TODO Save the modules and exponents into files (find file names on Project1 pdf)
+        //TODO Get the modulus and exponents for each RSA public and private keys
 
-    //TODO Take 16 characters from the user
+        //TODO Save the modules and exponents into files (find file names on Project1 pdf)
 
-    //TODO save the users 16 character input to file called symmetric.key
-    //This 16 character input will be used as the 128 bit AES symmetric key
+        //TODO Take 16 characters from the user
+
+        //TODO save the users 16 character input to file called symmetric.key
+        //This 16 character input will be used as the 128 bit AES symmetric key
+    }
 }
