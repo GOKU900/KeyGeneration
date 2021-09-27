0c9c39db95e65bf181c1f0231be9059f56045949
Goku900 <lizeth.a.nava@outlook.com>
2021-09-26 21:15:56 -0600

    KeyGenerator file with TODOs

---
+++ src/KeyGenerator.java
@@ -0,0 +1,14 @@
+public class KeyGenerator {
+    //TODO Create pair of RSA public and private keys for X
+
+    //TODO Create pair of RSA public and private keys for Y
+
+    //TODO Get the modulus and exponents for each RSA public and private keys
+
+    //TODO Save the modules and exponents into files (find file names on Project1 pdf)
+
+    //TODO Take 16 characters from the user
+
+    //TODO save the users 16 character input to file called symmetric.key
+    //This 16 character input will be used as the 128 bit AES symmetric key
+}
