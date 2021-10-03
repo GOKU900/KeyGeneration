70e56877578907e9d8018e3028d3334982807aa2
Goku900 <lizeth.a.nava@outlook.com>
2021-10-03 10:52:32 -0600

    Added code to get the modulus and exponents

--- src/KeyGenerator.java
+++ src/KeyGenerator.java
@@ -1,8 +1,10 @@
 import java.security.*;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 
 public class KeyGenerator{
     public static void main(String[] args) throws Exception {
-        //TODO Create pair of RSA public and private keys for X
+        //TODOd Create pair of RSA public and private keys for X
         SecureRandom random = new SecureRandom();
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(1024, random);
@@ -10,10 +12,19 @@ public class KeyGenerator{
         Key pubKeyX = pair.getPublic();
         Key privKeyX = pair.getPrivate();
 
-        //TODO Create pair of RSA public and private keys for Y
-
-        //TODO Get the modulus and exponents for each RSA public and private keys
-
+        //TODOd Create pair of RSA public and private keys for Y
+        SecureRandom randomTwo = new SecureRandom();
+        KeyPairGenerator generatorTwo = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(1024, randomTwo);
+        KeyPair pairTwo = generatorTwo.generateKeyPair();
+        Key pubKeyY = pair.getPublic();
+        Key privKeyY = pair.getPrivate();
+        //TODOd Get the modulus and exponents for each RSA public and private key
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        RSAPublicKeySpec publicKeySpecX = factory.getKeySpec(pubKeyX,RSAPublicKeySpec.class);
+        RSAPrivateKeySpec privateKeySpecX = factory.getKeySpec(privKeyX,RSAPrivateKeySpec.class);
+        RSAPublicKeySpec publicKeySpecY = factory.getKeySpec(pubKeyY,RSAPublicKeySpec.class);
+        RSAPrivateKeySpec privateKeySpecY = factory.getKeySpec(privKeyY,RSAPrivateKeySpec.class);
         //TODO Save the modules and exponents into files (find file names on Project1 pdf)
 
         //TODO Take 16 characters from the user
