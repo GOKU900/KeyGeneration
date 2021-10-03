4691a5b94291ba56d58273037b5faf35ff6e2a34
Goku900 <lizeth.a.nava@outlook.com>
2021-10-03 12:51:19 -0600

    Completed and working as expected

--- src/KeyGenerator.java
+++ src/KeyGenerator.java
@@ -1,8 +1,38 @@
+import java.io.*;
+import java.math.BigInteger;
 import java.security.*;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.Scanner;
 
 public class KeyGenerator{
+    //TODOd create method to save the modulus and exponents to files
+    public static void saveToFile(String fileName, BigInteger mod, BigInteger exp) throws IOException {
+
+        System.out.println("Write to " + fileName + ": modulus = " +
+                mod.toString() + ", exponent = " + exp.toString() + "\n");
+
+        ObjectOutputStream oout = new ObjectOutputStream(
+                new BufferedOutputStream(new FileOutputStream(fileName)));
+
+        try {
+            oout.writeObject(mod);
+            oout.writeObject(exp);
+        } catch (Exception e) {
+            throw new IOException("Unexpected error", e);
+        } finally {
+            oout.close();
+        }
+    }
+
+    public static void saveTextToFile(String filename,String userIn) throws FileNotFoundException {
+        System.out.println("Saving symmetric key to file Symmetric.key");
+
+        try (PrintWriter out = new PrintWriter(filename)) {
+            out.println(userIn);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         //TODOd Create pair of RSA public and private keys for X
         SecureRandom random = new SecureRandom();
@@ -15,21 +45,39 @@ public class KeyGenerator{
         //TODOd Create pair of RSA public and private keys for Y
         SecureRandom randomTwo = new SecureRandom();
         KeyPairGenerator generatorTwo = KeyPairGenerator.getInstance("RSA");
-        generator.initialize(1024, randomTwo);
+        generatorTwo.initialize(1024, randomTwo);
         KeyPair pairTwo = generatorTwo.generateKeyPair();
-        Key pubKeyY = pair.getPublic();
-        Key privKeyY = pair.getPrivate();
+        Key pubKeyY = pairTwo.getPublic();
+        Key privKeyY = pairTwo.getPrivate();
+
         //TODOd Get the modulus and exponents for each RSA public and private key
         KeyFactory factory = KeyFactory.getInstance("RSA");
-        RSAPublicKeySpec publicKeySpecX = factory.getKeySpec(pubKeyX,RSAPublicKeySpec.class);
-        RSAPrivateKeySpec privateKeySpecX = factory.getKeySpec(privKeyX,RSAPrivateKeySpec.class);
-        RSAPublicKeySpec publicKeySpecY = factory.getKeySpec(pubKeyY,RSAPublicKeySpec.class);
-        RSAPrivateKeySpec privateKeySpecY = factory.getKeySpec(privKeyY,RSAPrivateKeySpec.class);
-        //TODO Save the modules and exponents into files (find file names on Project1 pdf)
+        RSAPublicKeySpec publicKeySpecX = factory.getKeySpec(pubKeyX, RSAPublicKeySpec.class);
+        RSAPrivateKeySpec privateKeySpecX = factory.getKeySpec(privKeyX, RSAPrivateKeySpec.class);
+        RSAPublicKeySpec publicKeySpecY = factory.getKeySpec(pubKeyY, RSAPublicKeySpec.class);
+        RSAPrivateKeySpec privateKeySpecY = factory.getKeySpec(privKeyY, RSAPrivateKeySpec.class);
 
-        //TODO Take 16 characters from the user
+        //TODOd Save the modules and exponents into files (find file names on Project1 pdf)
+        saveToFile("XPublic.key", publicKeySpecX.getModulus(), publicKeySpecX.getPublicExponent());
+        saveToFile("XPrivate.key", privateKeySpecX.getModulus(), privateKeySpecX.getPrivateExponent());
+        saveToFile("YPublic.key", publicKeySpecY.getModulus(), publicKeySpecY.getPublicExponent());
+        saveToFile("YPrivate.key", privateKeySpecY.getModulus(), privateKeySpecY.getPrivateExponent());
 
-        //TODO save the users 16 character input to file called symmetric.key
+        //TODOd Take 16 characters from the user
+        String symmetricKey;
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Please provide a 16 character symmetric key: ");
+            symmetricKey = in.nextLine();
+            if (symmetricKey.length() != 16) {
+                System.out.println("You only provided " + symmetricKey.length() + " characters");
+            }
+        }
+        while (symmetricKey.length() != 16);
+
+
+        //TODOd save the users 16 character input to file called symmetric.key
         //This 16 character input will be used as the 128 bit AES symmetric key
+        saveTextToFile("symmetric.key",symmetricKey);
     }
 }
