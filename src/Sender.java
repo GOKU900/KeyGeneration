e09b1c29885833b90bebeaca7bda5b3907e510e0
Luciel <55259900+vLuciel@users.noreply.github.com>
2021-10-04 17:01:43 -0600

    1.0.7
    + Added SHA256 calculation method
    + Added SHA256 to Hexadecimal method
    
    - Still need ability to invert first byte
    - Still need to work out kinks though
    - Still need RSA encryption calculation
    - Still need AES encryption calculation

--- src/Sender.java
+++ src/Sender.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.io.*;
 
@@ -14,9 +18,10 @@ public class Sender {
 
         // basically says name of the file we need to grab
 
-        File M = new File(messageFile + ".txt");
+        File M = new File(messageFile);
             // since this is asking for a path and file name, this WILL vary
             // this also assumes M is a text file, so watch out
+            // 1 ascii char = 1 byte, make each part a small multiple of 1024 bytes
 
         Scanner fileReader = new Scanner(M);
             while (fileReader.hasNextLine()){
@@ -28,17 +33,40 @@ public class Sender {
 
 
         System.out.println("Do you want to invert the 1st byte in SHA256(M)? (y or n)");
-            otherInput = scan.nextLine();
+            otherInput = scan.next();
                 // could probably just be a Char
 
-            if(otherInput == "y"){
+            if(otherInput == "y" || otherInput == "Y"){
 
-            } else {
+            } else if (otherInput == "n" || otherInput == "N"){
 
             }
             // if yes, invert, else, go on.
 
         //Calculate RSA then AES (this should be a call from keygen class i think?)
+    }
+
+    public static byte[] SHAconvert(String input) throws NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    } // This is used to convert text to SHA
+
+    public static String toHex(byte[] hash){
+
+        BigInteger integer = new BigInteger(1,hash);
+
+        StringBuilder hexString = new StringBuilder(integer.toString(16));
+
+        while (hexString.length() < 32){
+
+            hexString.insert(0, '0');
+        }
+
+        // add invert first byte portion
+
+        return hexString.toString();
     }
 
 }
