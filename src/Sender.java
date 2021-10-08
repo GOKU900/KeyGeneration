1f9e53b5f8e342c5ec531a3561a0651ed5ded4e6
Goku900 <lizeth.a.nava@outlook.com>
2021-10-07 19:20:23 -0600

    Worked on SHA256 method, created keys, concat M and SHA256(M)

--- src/Sender.java
+++ src/Sender.java
@@ -1,34 +1,90 @@
 import java.math.BigInteger;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
 import java.util.*;
 import java.io.*;
 
 public class Sender {
+    private static int BUFFER_SIZE = 32*1024;
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        //get the modulus and exponent from the XPrivate.key file
+        //NOTE: when uploading to the class server we are going to need the entire file path
+        FileInputStream keyFile = new FileInputStream("XPrivate.key");
+        ObjectInputStream privateKeyObject = new ObjectInputStream(keyFile);
+        BigInteger modulus = (BigInteger) privateKeyObject.readObject();
+        BigInteger exponent = (BigInteger) privateKeyObject.readObject();
+        privateKeyObject.close();
+
+        //Create the private key spec
+        RSAPrivateKeySpec privateSpec = new RSAPrivateKeySpec(modulus,exponent);
+
+        //create a key factory
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+
+        //create the RSA private key
+        PrivateKey privateKeyX = factory.generatePrivate(privateSpec);
+
+        //create a symmetric key from symmetric.key file for AES algorithm
+        File symmetricKeyFile = new File("symmetric.key");
+        Scanner sc = new Scanner(symmetricKeyFile);
+        String sKey = sc.nextLine();
+        byte[] byteKey = new byte[128];
+        SecretKey secretKey = new SecretKeySpec(sKey.getBytes(StandardCharsets.UTF_8),"AES");
+        //Cipher cipher = Cipher.getInstance("AES");
+
         Scanner scan = new Scanner(System.in);
-
         String messageFileString;
-        String otherInput ;
-
-
+        //String otherInput ;
 
         System.out.print("Input the name of the message file: ");
         messageFileString = scan.nextLine();
+        String sha256Value = SHA256Convert(messageFileString);
+
+        //include the SHA256 value into the message file provided by user
+        FileWriter shaWriter = new FileWriter(messageFileString,true);
+        BufferedWriter bw = new BufferedWriter(shaWriter);
+        bw.write(sha256Value);
+        bw.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // basically says name of the file we need to grab
 
-            File M = new File(messageFileString);
-            Scanner mReader = new Scanner(M);
+        File M = new File(messageFileString);
+        //probably need to use BufferredInputStream here
+        Scanner mReader = new Scanner(M);
 
             while (mReader.hasNextLine()){
 
                 String data = mReader.nextLine();
                 System.out.println(data);
 
-                // Not sure if this should be a char or not yet.
+                // Not sure if this should be a char or not yet. LAN: I think this is a byte array not a char array
                 // Will have to read piece by piece.
             }
 
@@ -47,7 +103,7 @@ public class Sender {
 
             try{
                 FileWriter ddWrite = new FileWriter("message.dd");
-                ddWrite.write("Test writing. Must output SHA.");
+                ddWrite.write(sha256Value);
                 ddWrite.close();
 
                 System.out.println("File write successful.");
@@ -89,12 +145,20 @@ public class Sender {
 
 
 
-    public static byte[] SHAconvert(String input) throws NoSuchAlgorithmException {
-
+    public static String SHA256Convert(String messageInput) throws NoSuchAlgorithmException, IOException {
+        BufferedInputStream file = new BufferedInputStream(new FileInputStream(messageInput));
         MessageDigest md = MessageDigest.getInstance("SHA-256");
+        DigestInputStream in = new DigestInputStream(file,md);
+        int i;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        do{
+            i = in.read(buffer,0,BUFFER_SIZE);
+        } while(i == BUFFER_SIZE);
+        md = in.getMessageDigest();
+        in.close();
 
-        return md.digest(input.getBytes(StandardCharsets.UTF_8));
-    } // This is used to convert text to SHA
+        return new String(md.digest());
+    } // This is used to convert message to SHA256
 
     public static String toHex(byte[] hash){
 
